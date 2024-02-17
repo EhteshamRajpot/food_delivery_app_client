cb3f848e4529da8560c1f1964a2c55fe62173d27
Muhammad Ehtesham <muhammadehtesham69@gmail.com>
2024-02-18 00:23:53 +0500

    first commit

--- src/shared/Auth/ForgotPassword.tsx
+++ src/shared/Auth/ForgotPassword.tsx
@@ -1,60 +1,86 @@
-import { LOGIN_USER } from '@/src/graphql/actions/login.action';
-import styles from '@/src/utils/style';
-import { useMutation } from '@apollo/client';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import styles from "@/src/utils/style";
 import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import toast from "react-hot-toast";
+import { useMutation, useQuery } from "@apollo/client";
+import { FORGOT_PASSWORD } from "@/src/graphql/actions/forgot-password.action";
 
-const ForgotPasswordSchema = z.object({
-    email: z.string().email(),
-})
+const formSchema = z.object({
+  email: z.string().email(),
+});
 
-type ForgotPasswordSchema = z.infer<typeof ForgotPasswordSchema>
+type ForgotPasswordSchema = z.infer<typeof formSchema>;
 
-const ForgotPassword = ({ setActiveState }: { setActiveState: (e: string) => void}) => {
-    const [Login, { loading }] = useMutation(LOGIN_USER)
-    const { register, handleSubmit, formState: { errors, isSubmitting }, reset } = useForm<ForgotPasswordSchema>({
-        resolver: zodResolver(ForgotPasswordSchema)
-    })
- 
-    const onSubmit = async (data: ForgotPasswordSchema) => {
-       console.log(data)
+const ForgotPassword = ({
+  setActiveState,
+}: {
+  setActiveState: (e: string) => void;
+}) => {
+  const [forgotPassword, { loading }] = useMutation(FORGOT_PASSWORD);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<ForgotPasswordSchema>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = async (data: ForgotPasswordSchema) => {
+    try {
+      const response = await forgotPassword({
+        variables: {
+          email: data.email,
+        },
+      });
+      toast.success("Please check your email to reset your password!");
+      reset();
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.message);
     }
-    return (
-        <div>
-            <h1 className={`${styles.title}`}>Forgot your password?</h1>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label className={`${styles.label}`}>Enter your Email</label>
-                <input
-                    {...register("email")}
-                    type="email"
-                    placeholder="loginmail@gmail.com"
-                    className={`${styles.input}`}
-                />
-                {errors.email && (
-                    <span className="text-red-500 block mt-1">
-                        {`${errors.email.message}`}
-                    </span>
-                )}
-                <br/>
-                  <input
-                        type="submit"
-                        value="Submit"
-                        disabled={isSubmitting}
-                        className={`${styles.button} mt-3`}
-                    />
-                <h5 className='text-center pt-4 font-Poppins text-[14px]'>
-                    Or Go Back To
-                    <span
-                        className="text-[#2190ff] pl-1 cursor-pointer"
-                        onClick={() => setActiveState("Login")}
-                    >
-                        Login
-                    </span>
-                </h5>
-            </form>
-        </div>
-    )
-}
+  };
 
-export default ForgotPassword
+  return (
+    <div>
+      <h1 className={`${styles.title}`}>Forgot your password?</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label className={`${styles.label}`}>Enter your Email</label>
+        <input
+          {...register("email")}
+          type="email"
+          placeholder="loginmail@gmail.com"
+          className={`${styles.input}`}
+        />
+        {errors.email && (
+          <span className="text-red-500 block mt-1">
+            {`${errors.email.message}`}
+          </span>
+        )}
+        <br />
+        <br />
+        <input
+          type="submit"
+          value="Submit"
+          disabled={isSubmitting || loading}
+          className={`${styles.button} mt-3`}
+        />
+        <br />
+        <h5 className="text-center pt-4 font-Poppins text-[14px]">
+          Or Go Back to
+          <span
+            className="text-[#2190ff] pl-1 cursor-pointer"
+            onClick={() => setActiveState("Login")}
+          >
+            Login
+          </span>
+        </h5>
+        <br />
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
